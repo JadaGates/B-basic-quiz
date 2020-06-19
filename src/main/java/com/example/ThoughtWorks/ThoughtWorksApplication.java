34cc41b7d49e1970a6986d22ae0e74e590c99019
jingx <xin.jing1@thoughtworks.com>
2020-06-19 21:07:27 +0800

    [xin ] first commit: initial

---
+++ src/main/java/com/example/ThoughtWorks/ThoughtWorksApplication.java
@@ -0,0 +1,13 @@
+package com.example.ThoughtWorks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThoughtWorksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThoughtWorksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ThoughtWorks/ThoughtWorksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ThoughtWorks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThoughtWorksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
