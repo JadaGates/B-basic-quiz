d0a7d04c54bc40d06db8a975790871e13b5926eb
jingx <xin.jing1@thoughtworks.com>
2020-06-21 01:02:35 +0800

    [xin] allows cors

--- src/main/java/com/example/ThoughtWorks/ThoughtWorksApplication.java
+++ src/main/java/com/example/ThoughtWorks/ThoughtWorksApplication.java
@@ -2,12 +2,25 @@ package com.example.ThoughtWorks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ThoughtWorksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ThoughtWorksApplication.class, args);
+	}
+
+	@RestController
+	@CrossOrigin(origins = "http://localhost:1234")
+	public class DemoApplication {
+
+		@GetMapping("/h")
+		public String hello() {
+			return "Hello World!";
+		}
 	}
 
 }
