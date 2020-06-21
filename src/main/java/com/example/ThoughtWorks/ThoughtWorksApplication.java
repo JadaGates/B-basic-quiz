1b75d98dceace2792ced4ec9e37ad6cd8bb8170e
jingx <xin.jing1@thoughtworks.com>
2020-06-21 09:35:12 +0800

    [xin] return with pathvarible

--- src/main/java/com/example/ThoughtWorks/ThoughtWorksApplication.java
+++ src/main/java/com/example/ThoughtWorks/ThoughtWorksApplication.java
@@ -2,8 +2,10 @@ package com.example.ThoughtWorks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -17,9 +19,10 @@ public class ThoughtWorksApplication {
 	@CrossOrigin(origins = "http://localhost:1234")
 	public class DemoApplication {
 
-		@GetMapping("/h")
-		public String hello() {
-			return "Hello World!";
+		@GetMapping("/user/{id}")
+		public String hello(@PathVariable String id) {
+			String result = "Hello User " + id;
+			return result;
 		}
 	}
 
