54afc26f091a886297d1256ff2022d199e535071
jingx <xin.jing1@thoughtworks.com>
2020-06-21 23:36:36 +0800

    [xin] add more data to user1 to show

--- src/main/java/com/example/ThoughtWorks/service/AccountEducationService.java
+++ src/main/java/com/example/ThoughtWorks/service/AccountEducationService.java
@@ -17,9 +17,17 @@ public class AccountEducationService {
 
     public AccountEducationService(){
         ArrayList<AccountEducation> accountEducationData = new ArrayList<>();
+        String decriptionOne = "Vis at rationibus elaboraret reprimique. " +
+                "Salutatus temporibus te mel, iisque ancillae perfecto te has, ea vidisse offendit has.";
 
-        accountEducationData.add(new AccountEducation((long) 1995, "npu", "htt", (long) 1));
-        accountEducationData.add(new AccountEducation((long) 2000, "npu", "8909", (long) 1));
+        String decriptionTwo = "Nam dolorem suavitate ex, mel simul voluptatibus eu. Sit doctus repudiare incorrupte ei.";
+
+        accountEducationData.add(new AccountEducation((long) 1995, "I was born in Canada", decriptionOne, (long) 1));
+        accountEducationData.add(new AccountEducation((long) 2000, "Fly High in the sky", decriptionTwo, (long) 1));
+        accountEducationData.add(new AccountEducation((long) 2006, "I was born in Canada", decriptionOne, (long) 1));
+        accountEducationData.add(new AccountEducation((long) 2010, "Fly High in the sky", decriptionTwo, (long) 1));
+        accountEducationData.add(new AccountEducation((long) 2015, "I was born in Canada", decriptionOne, (long) 1));
+        accountEducationData.add(new AccountEducation((long) 2020, "Fly High in the sky", decriptionTwo, (long) 1));
 
         accountEducationMap.put(1, accountEducationData);
 
@@ -28,6 +36,7 @@ public class AccountEducationService {
     public List<AccountEducation> getAllAccountEducation(Integer id){
         return new ArrayList<>(accountEducationMap.get(id));
     }
+
     @JsonIgnoreProperties(value = { "userId" })
     public void updateAccountEducation(AccountEducation accountEducation, Integer id){
         if(accountEducationMap.containsKey(id)){
--- src/main/java/com/example/ThoughtWorks/service/AccountService.java
+++ src/main/java/com/example/ThoughtWorks/service/AccountService.java
@@ -5,27 +5,36 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
 public class AccountService {
-    private Map<Integer, Account> accountMap= new HashMap<>();
+    private Map<Integer, Account> accountMap = new HashMap<>();
 
-    @JsonIgnoreProperties(value = { "id" })
-    public AccountService(){
-        accountMap.put(1, new Account(1, "Flivo", (long) 24, "htt", "jjj"));
-        accountMap.put(2, new Account(2, "Tommy", (long) 34, "445", "fdfj"));}
+    @JsonIgnoreProperties(value = {"id"})
+    public AccountService() {
+        String descriptionUserOne = "Self-introductions are hard because most people don’t go around every day thinking about who they are, " +
+                "let alone talking about it to complete strangers. So when you are put on the spot during a job interview";
+        String descriptionUserTwo = "The good news is that most of us are really overthinking it. " +
+                "Self-introduction, or these tell-me-about-yourself questions, are actually quite simple to answer.";
 
-        public int creatAccount(Account account){
+        accountMap.put(1, new Account(1, "Flivo", (long) 24, "https://s1.ax1x.com/2020/06/12/tOIErR.jpg", descriptionUserOne));
+        accountMap.put(2, new Account(2, "Tommy", (long) 34, "https://s1.ax1x.com/2020/06/12/tOIAM9.jpg", descriptionUserTwo));
+    }
+
+    public int creatAccount(Account account) {
         int userId = generateAccountId(accountMap);
         account.setId(userId);
         accountMap.put(userId, account);
         return accountMap.size();
     }
-    public int generateAccountId(Map accountMap){
+
+    public int generateAccountId(Map accountMap) {
         return accountMap.size() + 1;
     }
-    public Account getAccountById(Integer id){
+
+    public Account getAccountById(Integer id) {
         return accountMap.get(id);
-}
+    }
 }
